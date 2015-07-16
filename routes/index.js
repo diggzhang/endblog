937f8f33076b6cc76f9771918da2d9a9f08e6c81
diggzhang <diggzhang@gmail.com>
2015-07-16 09:40:56 +0800

    Add post function and markdown editor

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
+var crypto = require('crypto'),
+	User = require('../models/user.js'),
+	Post = require('../models/post.js');
 var express = require('express');
 var router = express.Router();
 
@@ -8,36 +11,154 @@ var router = express.Router();
 
 module.exports = function(app) {
 	app.get('/', function (req, res) {
-		res.render('index', { title: 'Express' });
+		Post.get(null, function (err, posts) {
+			if (err) {
+				posts = [];
+			}
+
+			res.render('index', {
+				title: 'HomePage',
+				user: req.session.user,
+				posts: posts,
+				success: req.flash('success').toString(),
+				error: req.flash('error').toString()
+			});
+		});
 	});
+
+
+
 
 	//app.get('/go', function (req, res) {
 	//	res.send('Hello World');
 	//});
 
 	// login reg post logout
+	app.get('/reg', checkNotLogin);
 	app.get('/reg', function (req, res) {
-		res.render('reg', { title: '注册'} );
+		res.render('reg', {
+			title: '注册',
+			user: req.session.user,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString()
+		});
 	});
+
 	app.post('/reg', function (req, res) {
+		var name = req.body.name,
+			password = req.body.password,
+			password_re = req.body['password-repeat'];
+
+		if (password_re != password) {
+			req.flash('error', '两次输入密码不一致');
+			return res.redirect('/reg');
+		}
+
+		var md5 = crypto.createHash('md5'),
+			password = md5.update(req.body.password).digest('hex');
+
+		var newUser = new User({
+			name: req.body.name,
+			password: password,
+			email: req.body.email
+		});
+
+		User.get(newUser.name, function (err, user) {
+			if (user) {
+				req.flash('error', '用户已存在');
+				return res.redirect('/reg');
+			}
+
+			newUser.save(function (err, user) {
+				if (err) {
+					req.flash('error', err);
+					return res.redirect('/reg');
+				}
+
+				req.session.user = user;
+				req.flash('success', ' 注册成功');
+				res.redirect('/');
+			});
+		});
 	});
 
+	app.get('/login', checkNotLogin);
 	app.get('/login', function (req, res) {
-		res.render('login', { title: '登录'} );
-	})
+		res.render('login', {
+			title: '登录',
+			user: req.session.user,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString()
+		});
+	});
+
+	app.get('/login', checkNotLogin);
 	app.post('/login', function (req, res) {
-	})
+		var md5 = crypto.createHash('md5'),
+			password = md5.update(req.body.password).digest('hex');
+		User.get(req.body.name, function (err, user) {
+			if (!user) {
+				req.flash('error', 'This User is Missing in Universe!');
+				return res.redirect('/login');
+			}
 
-	app.get('/logout', function (req, res) {
-		res.render('logout', {title:'登出'} );
-	});
-	app.post('/logout', function (req, res) {
+			if (user.password != password) {
+				req.flash('error', 'PassWord Error!');
+				return res.redirect('/login');
+			}
+
+			req.session.user = user;
+			req.flash('success', '登录成功');
+			res.redirect('/');
+		})
 	});
 
+	app.get('/post', checkLogin);
 	app.get('/post', function (req, res) {
-		res.render('post', { title: '发布'} );
+		res.render('post', {
+			title: '发表',
+			user: req.session.user,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString()
+		});
 	});
+
+	app.post('/post', checkLogin);
 	app.post('/post', function (req, res) {
+		var currentUser = req.session.user,
+			post = new Post(currentUser.name, req.body.title, req.body.post);
+
+		post.save(function (err) {
+			if(err) {
+				req.flash('error', err);
+				return res.redirect('/');
+			}
+			req.flash('success', '发布成功');
+			res.redirect('/');
+		});
 	});
+
+	app.post('/logout', checkLogin);
+	app.get('/logout', function (req, res) {
+		req.session.user = null;
+		req.flash('success','Logout Over :D')
+		res.redirect('/');
+	});
+
+	function checkLogin(req, res, next) {
+		if (!req.session.user) {
+			req.flash('error', '未登录! function checkLogin(){}');
+			res.redirect('/login');
+		}
+		next();
+	}
+
+	function checkNotLogin(req, res, next) {
+		if (req.session.user) {
+			req.flash('error', '已登录! function checkNotLogin(){}');
+			res.redirect('back')
+		}
+		next();
+	}
 
 };
