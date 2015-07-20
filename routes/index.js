2f34c1df9b528fcb1821847ff7181be461f77b3a
diggzhang <diggzhang@gmail.com>
2015-07-20 14:16:36 +0800

    add pre or next page function, change functiin getAll as getTen

--- routes/index.js
+++ routes/index.js
@@ -12,15 +12,21 @@ var router = express.Router();
 
 module.exports = function(app) {
 	app.get('/', function (req, res) {
-		Post.getAll(null, function (err, posts) {
+
+		var page = req.query.p ? parseInt(req.query.p) : 1;
+
+		Post.getTen(null, page,function (err, posts, total) {
 			if (err) {
 				posts = [];
 			}
 
 			res.render('index', {
 				title: 'HomePage',
-				user: req.session.user,
 				posts: posts,
+				page: page,
+				isFirstPage: (page - 1) == 0,
+				isLastPage: ((page - 1) * 10 + posts.length) == total,
+				user: req.session.user,
 				success: req.flash('success').toString(),
 				error: req.flash('error').toString()
 			});
@@ -157,14 +163,18 @@ module.exports = function(app) {
 		res.redirect('/upload');
 	});
 
-	app.get('/u/:name', function (req,res) {
+	app.get('/u/:name', function (req,  res) {
+
+		var page = req.query.p ? parseInt(req.query.p) : 1;
+
 		User.get(req.params.name, function (err, user) {
 			if (!user) {
 				req.flash('error', 'User Missing in Universe :(');
 				return res.redirect('/');
 			};
 
-			Post.getAll(user.name, function (err, posts) {
+			Post.getTen(user.name, page,function (err, posts, total) {
+
 				if (err) {
 					req.flash('error', err);
 					return res.redirect('/');
@@ -173,6 +183,9 @@ module.exports = function(app) {
 				res.render('user', {
 					title: user.name,
 					posts: posts,
+					page: page,
+					isFirstPage: (page - 1) == 0,
+					isLastPage: ((page - 1) * 10 + posts.length) == total,
 					user: req.session.user,
 					success: req.flash('success').toString(),
 					error: req.flash('error').toString()
