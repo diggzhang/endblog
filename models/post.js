7fc8282834d0063b1c7d4aa8a971dcd1286f05e3
diggzhang <diggzhang@gmail.com>
2015-07-21 13:49:31 +0800

    add search function

--- models/post.js
+++ models/post.js
@@ -28,7 +28,8 @@ Post.prototype.save = function(callback) {
 		title: this.title,
 		tags: this.tags,
 		post: this.post,
-		comments: []
+		comments: [],
+		pv: 0
 	};
 
 	mongodb.open(function (err, db) {
@@ -148,21 +149,31 @@ Post.getOne = function (name, day, title, callback) {
 				"time.day": day,
 				"title": title
 			}, function (err, doc) {
-				mongodb.close();
 				if (err) {
+					mongodb.close();
 					return callback(err);
 				};
 
-			//	doc.post = markdown.toHTML(doc.post);
-
 				if (doc) {
+					collection.update({
+						"name": name,
+						"time.day": day,
+						"title": title
+					}, {
+						$inc: { "pv": 1}
+					}, function (err) {
+						mongodb.close();
+						if (err) {
+							return callback(err);
+						};
+					});
+
 					doc.post = markdown.toHTML(doc.post);
 					doc.comments.forEach(function (comment) {
 						comment.content = markdown.toHTML(comment.content);
 					});
+					callback(null, doc)
 				};
-
-				callback(null, doc);
 			});
 		});
 	});
@@ -337,6 +348,39 @@ Post.getTag = function (tag, callback) {
 				};
 
 				callback(null, docs)
+			});
+		});
+	});
+};
+
+Post.search = function (keyword, callback) {
+
+	mongodb.open(function (err, db) {
+		if (err) {
+			return callback(err);
+		};
+
+		db.collection('posts', function (err, collection) {
+			if (err) {
+				mongodb.close();
+				return callback(err);
+			};
+
+			var pattern = new RegExp(keyword, "i");
+			collection.find({
+				"title": pattern
+			}, {
+				"name": 1,
+				"time": 1,
+				"title": 1
+			}).sort({
+				time: -1
+			}).toArray(function (err, docs) {
+				mongodb.close();
+				if (err) {
+					return callback(err);
+				}
+				callback(null, docs);
 			});
 		});
 	});
